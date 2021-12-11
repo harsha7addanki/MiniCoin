3afec29628bf2cce309c4ff5af2a584b29746fe1
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-11 07:47:09 -0500

    tuf

--- Wallet/models.py
+++ Wallet/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, password=None):
+        user = self.model()
+
+        user.set_password(password)
+        user.set_coins(50)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email,  password):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.set_coins(100)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 class User(AbstractUser):
     coins = models.BigIntegerField()
