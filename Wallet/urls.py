56328ecc1d0b2b15873ef0af32d574f432313a54
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-11 09:33:52 -0500

    yety

--- Wallet/urls.py
+++ Wallet/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signup,viewWallet
+from .views import signup,viewWallet,transfer
 
 urlpatterns = [
     path("signup/",signup,name="signup"),
-    path("",viewWallet,name="home")
+    path("",viewWallet,name="home"),
+    path("transfer/",transfer,name="transfer"),
 ]
