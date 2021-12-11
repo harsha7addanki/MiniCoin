d99c317869a478cb3a7583590497517cf9584e67
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-11 09:27:31 -0500

    hi

--- Wallet/forms.py
+++ Wallet/forms.py
@@ -1,8 +1,29 @@
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import *
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User 
         fields = ('username','password1', 'password2',)
+
+class TransferForm(forms.Form):
+    to = forms.CharField(required=True)
+    amount = forms.IntegerField(required=True)
+
+    def clean_amount(self):
+        data = self.cleaned_data['amount']
+        if data <= 0:
+            raise ValidationError(_('Invalid amount - must be greater than 0'))
+        else: 
+            return data
+
+    def clean_to(self):
+        data = self.cleaned_data['to']
+        try:
+            user = User.objects.exclude(pk=self.instance.pk).get(username=data)
+        except User.DoesNotExist:
+            raise ValidationError(u'Username "%s" is already in use.' % data)    
+        return data
--- Wallet/views.py
+++ Wallet/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import login, authenticate
+from .models import User
+from Wallet.forms import TransferForm
 from .forms import SignUpForm
 from django.shortcuts import render, redirect
 
@@ -23,3 +25,21 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def transfer(request):
+    if request.method == 'POST':
+        form = TransferForm(request.POST)
+        if form.is_valid():
+            form.save()
+            to = form.cleaned_data.get('to')
+            ammount = form.cleaned_data.get('ammount')
+            fromuser = request.user
+            if fromuser.ammount < ammount:
+                return redirect('home')
+            touser = User.objects.get(username=to)
+            fromuser.update(ammount=fromuser.ammount-ammount)
+            touser.update(ammount=touser+ammount)
+            return redirect('home')
+    else:
+        form = TransferForm()
+    return render(request, 'transfer.html', {'form': form})
