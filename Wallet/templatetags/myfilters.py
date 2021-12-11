34220f2bf5214133c02b69c1f7d91d5b6ae71b4c
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-11 07:34:26 -0500

    t

--- Wallet/templatetags/myfilters.py
+++ Wallet/templatetags/myfilters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='addclass')
 def addclass(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+@register.filter(name='labeld')
+def labeld(value, arg):
+    return value.as_widget(attrs={'aria-describedby': arg})
