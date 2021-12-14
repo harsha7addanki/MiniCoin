5d5c03ed53791580f1af0989fb92867f74130d04
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-13 21:28:11 -0500

    hehehoho

--- Wallet/templatetags/myfilters.py
+++ Wallet/templatetags/myfilters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='addclass')
 def addclass(value, arg):
     return value.as_widget(attrs={'aria-describedby': 'basic-addon1','class': arg})
+
+@register.filter(name='addclassi')
+def addclass(value, arg):
+    return value.as_widget(attrs={'aria-describedby': 'basic-addon1','class': arg, 'id': "yes"})
