fd666759051a8844bb5c374e97625b13860e571e
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-14 21:32:59 -0500

    hilol

--- Wallet/models.py
+++ Wallet/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+
+# User
 class UserManager(BaseUserManager):
     def create_user(self, password=None):
         user = self.model(
@@ -25,3 +28,9 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     coins = models.BigIntegerField(default=100)
     following = models.ManyToManyField("self", blank=True)
+    gifts = models.ManyToManyField('Gift')
+
+class Gift(models.Model):
+    from_usr = models.ForeignKey(User, models.CASCADE)
+    amount = models.BigIntegerField(default=10)
+    message = models.TextField()
