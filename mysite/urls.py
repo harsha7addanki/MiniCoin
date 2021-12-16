316cc41459856e81b3501732738372b570c5acdb
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-15 21:32:36 -0500

    grappeli

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('',include("Wallet.urls")),
     path('',include("django.contrib.auth.urls"))
