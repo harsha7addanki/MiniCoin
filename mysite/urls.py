f6582c1706d3bd9b84cdaaa56506706e570f7d60
Harsha Addanki <63471542+harsha7addanki@users.noreply.github.com>
2021-12-16 15:36:25 -0500

    lollypop

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('',include("Wallet.urls")),
     path('',include("django.contrib.auth.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
