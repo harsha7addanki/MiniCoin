a840262dd28dfa73effe7ebd89fdc0756eb808cd
Harsha Addanki <harsha7addanki@gmail.com>
2021-12-17 15:39:24 -0500

    he

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mysite.Wallet.WalletConfig',
+    'Wallet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
