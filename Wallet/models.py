db68d92a2b676df7953563d82b4cc24ab3067e66
Harsha Addanki <harsha7addanki@gmail.com>
2021-12-16 21:57:47 -0500

    x

--- Wallet/models.py
+++ Wallet/models.py
@@ -24,19 +24,19 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-class Gift(models.Model):
-    from_usr = models.ForeignKey("User", models.CASCADE)
-    amount = models.BigIntegerField(default=10)
-    message = models.TextField()
+# class Gift(models.Model):
+#     from_usr = models.ForeignKey("User", models.CASCADE)
+#     amount = models.BigIntegerField(default=10)
+#     message = models.TextField()
 
-class TestModel(models.Model):
-    name = models.TextField()
-    msg = models.TextField()
+# class TestModel(models.Model):
+#     name = models.TextField()
+#     msg = models.TextField()
 
-class User(AbstractUser):
-    coins = models.BigIntegerField(default=100)
-    following = models.ManyToManyField("self", blank=True)
-    #gifts = models.ManyToManyField('Gift', blank=True)
+# class User(AbstractUser):
+#     coins = models.BigIntegerField(default=100)
+#     following = models.ManyToManyField("self", blank=True)
+#     #gifts = models.ManyToManyField('Gift', blank=True)
 
 
 
