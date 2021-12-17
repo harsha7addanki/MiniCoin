78fd57433d72e99cbe4076d93a0a63e3f1d914a8
Harsha Addanki <harsha7addanki@gmail.com>
2021-12-17 17:36:18 -0500

    tyer

--- Wallet/urls.py
+++ Wallet/urls.py
@@ -3,7 +3,7 @@ from .views import signup,viewWallet,transfer,userpage,collectgift
 
 urlpatterns = [
     path("signup/",signup,name="signup"),
-    path("",viewWallet,name="home"),
+    # path("",viewWallet,name="home"),
     path("transfer/",transfer,name="transfer"),
     path("account/<str:username>/",userpage),
     path("collectgift/<int:id>/",collectgift,name="colgift"),
