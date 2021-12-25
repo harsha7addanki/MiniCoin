b85981bc4210772a60615c827b31791ebc72345a
Harsha Addanki <harsha7addanki@gmail.com>
2021-12-25 13:15:26 -0500

    my fault i removed the login

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from . import settings
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include("Wallet.urls")),
+    path('',include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
