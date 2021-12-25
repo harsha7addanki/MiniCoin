4db5093448359648b3a9d7d7d87c9c38abaffe29
Harsha Addanki <harsha7addanki@gmail.com>
2021-12-25 13:21:02 -0500

    I Think Tis Will Work

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+import django
 from . import settings
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include("Wallet.urls")),
-    path('',include("django.contrib.auth.urls")),
+    path('',django.contrib.auth.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
